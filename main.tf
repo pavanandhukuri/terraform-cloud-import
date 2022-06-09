c4b23fd71f465010aa39d80d3b1947cd72419852
Pavan Andhukuri <pavan496@gmail.com>
2022-06-09 19:37:21 +0530

    Add s3 bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "ap-south-1"
 }
+resource "aws_s3_bucket" "b" {
+  bucket = "pavan-terraform-import-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
